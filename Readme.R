33e929ddb490de796c7384b70e0511e9e82f5b2c
Péter Kalicz <kaliczp@gmail.com>
2022-10-19 18:08:28 +0200

    Monthly and yearly averages

--- Readme.R
+++ Readme.R
@@ -2,3 +2,6 @@ rawCece <- read.table("HABP_1RD_20020101_20211231_36703.csv", head = TRUE, sep =
 CeceDate <- as.Date(as.character(rawCece$Time), format = "%Y%m%d")
 library(xts)
 CeceRain <- xts(rawCece$r, CeceDate)
+
+apply.monthly(CeceRain, sum)
+apply.yearly(CeceRain, sum)
