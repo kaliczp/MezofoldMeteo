b6be97999ba7c6e8ad7130497d8ab3f535be32df
Péter Kalicz <kaliczp@gmail.com>
2022-10-19 18:07:18 +0200

    Time series creation

--- Readme.R
+++ Readme.R
@@ -1,1 +1,4 @@
 rawCece <- read.table("HABP_1RD_20020101_20211231_36703.csv", head = TRUE, sep = ";", skip = 5)
+CeceDate <- as.Date(as.character(rawCece$Time), format = "%Y%m%d")
+library(xts)
+CeceRain <- xts(rawCece$r, CeceDate)
