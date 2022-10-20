9d735af02b2931cde153205f0f8899c946000999
Péter Kalicz <kaliczp@gmail.com>
2022-10-20 06:59:58 +0200

    Read by URL

--- Readme.R
+++ Readme.R
@@ -1,4 +1,11 @@
-rawCece <- read.table("HABP_1RD_20020101_20211231_36703.csv", head = TRUE, sep = ";", skip = 5)
+stationids <- read.table("stationids.txt")
+filenames <- paste0("HABP_1RD_", stationids[,1], "_20020101_20211231_hist.zip")
+urlhist <- "https://odp.met.hu/climate/observations_hungary/daily_rain/historical/"
+temp <- tempfile()
+download.file(paste0(urlhist, filenames[2]), temp)
+rawCece <- read.table(unzip(temp), head = TRUE, sep = ";", skip = 5)
+unlink(temp)
+
 CeceDate <- as.Date(as.character(rawCece$Time), format = "%Y%m%d")
 library(xts)
 CeceRain <- xts(rawCece$r, CeceDate)
